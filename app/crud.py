3dc7e64d357ef4a06f66431dfcc272036d7783fc
Kylecyx <kylecyx@gmail.com>
2026-03-18 21:05:47 +0800

    bug fix

--- app/crud.py
+++ app/crud.py
@@ -2,4 +2,14 @@ from sqlalchemy.orm import Session
 from . import models
 
 def create_task(db: Session, title: str, description: str):
-    new_task = models.Task(title, description)
+    new_task = models.Task(
+        title = title, 
+        description = description
+    )
+    db.add(new_task)
+    db.commit()
+    db.refresh(new_task)
+    return new_task
+
+def get_tasks(db: Session):
+    return db.query(models.Task).all()
--- app/main.py
+++ app/main.py
@@ -1,11 +1,27 @@
 from fastapi import FastAPI
 from .database import engine
 from . import models
+from fastapi import Depends
+from sqlalchemy.orm import Session
+from .database import SessionLocal
+from . import crud
 
 app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 def root():
     return {"message": "Todo API is running"}
 
+@app.get("/tasks")
+def read_tasks(db: Session = Depends(get_db)):
+    return crud.get_tasks
+
 models.Base.metadata.create_all(bind=engine)
+
