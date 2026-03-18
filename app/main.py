82aafa57b538b41f5c410c940f2da91d84e23328
Kylecyx <kylecyx@gmail.com>
2026-03-18 20:30:26 +0800

    add task model

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from .database import engine
+from . import models
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "Todo API is running"}
+
+models.Base.metadata.create_all(bind=engine)
